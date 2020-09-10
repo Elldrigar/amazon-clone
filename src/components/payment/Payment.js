37a978b9ef0a613ca69c53141147043631ea21ff
Artur Gawron <artur@gawron.me>
2020-09-11 01:05:04 +0200

    Create payment component

--- src/components/payment/Payment.js
+++ src/components/payment/Payment.js
@@ -1,8 +1,55 @@
 import React from 'react';
 import './payment.scss';
+import { useStateValue } from '../../StateProvider';
+import CheckoutProduct from '../checkout-product/CheckoutProduct';
+import { Link } from 'react-router-dom';
 
 function Payment() {
-  return <div></div>;
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  return (
+    <div className="payment">
+      <div className="payment__container">
+        <h1 className="payment__checkoutCount">
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Delivery Address</h3>
+          </div>
+          <div className="payment__address">
+            <p>{user?.email}</p>
+            <p>21 cartoon box</p>
+            <p>Falseland, PL</p>
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Review items and delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
+        </div>
+
+        <div className="payment__section">
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">{/* STRIPE */}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Payment;
