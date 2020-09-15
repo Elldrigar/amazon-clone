8f20cea8e0af20f4fa4837394248451dbb5124b4
Artur Gawron <artur@gawron.me>
2020-09-15 23:48:11 +0200

    Add backend payment for stripe

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,31 @@
 const functions = require('firebase-functions');
 const express = require('express');
-const corse = require('cors');
-const stripe = require('stripe');
+const cors = require('cors');
+const { request } = require('express');
+const stripe = require('stripe')(
+  'sk_test_51HQbJzLzmAaGAOaO1daqCX9reHtXkVWgfhYNYC6AjGr6gnAqHkR8Np1Or7Cli9ChzFZwY7BlpVpEgJENjnaNrH0H00TeVivSCd'
+);
+
+//API
+
+const app = express();
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+
+app.post('/payment/create', async (request, response) => {
+  const total = request.query.total;
+
+  console.log('payment request recived!', total);
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: total,
+    currency: 'usd',
+  });
+
+  response.status(201).send({
+    clientSecret: paymentIntent.client_secret,
+  });
+});
+
+exports.api = functions.https.onRequest(app);
