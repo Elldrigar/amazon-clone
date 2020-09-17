283c5dc4baa5f1d810bdfffef00f9764d8f7e2d8
Artur Gawron <artur@gawron.me>
2020-09-17 15:20:25 +0200

    Prepare for first Deploy

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,12 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5001/clone-febbe/us-central1/api',
+  baseURL: 'https://us-central1-clone-febbe.cloudfunctions.net/api',
 });
 
 export default instance;
+
+// Local URL
+///////////
+//
+//   'http://localhost:5001/clone-febbe/us-central1/api'
